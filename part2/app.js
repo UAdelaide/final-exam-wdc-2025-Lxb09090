691dd856f23b0d153ce969b80e071611131437f5
WDC Student (missing default git user name) <unknown@student.adelaide.edu.au>
2025-06-20 10:02:58 +0000

    [automatic commit] 2025-06-20 10:02:58 UTC

--- part2/app.js
+++ part2/app.js
@@ -1,20 +1,26 @@
+// app.js
 const express = require('express');
 const path = require('path');
-require('dotenv').config();
-
+const session = require('express-session');
 const app = express();
 
-// Middleware
 app.use(express.json());
-app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+  secret: 'dogwalk-secret-key',
+  resave: false,
+  saveUninitialized: false
+}));
 
 // Routes
-const walkRoutes = require('./routes/walkRoutes');
-const userRoutes = require('./routes/userRoutes');
+const userRoutes = require('./userRoutes');
+const walkRoutes = require('./walkRoutes');
+const loginRoutes = require('./loginRoutes');
 
-app.use('/api/walks', walkRoutes);
 app.use('/api/users', userRoutes);
+app.use('/api/walks', walkRoutes);
+app.use('/', loginRoutes);
 
-// Export the app instead of listening here
-module.exports=app;
+module.exports = app;
 
