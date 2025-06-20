4d1aa54ebcc12bbe85da642907e38dde5fce70e6
WDC Student (missing default git user name) <unknown@student.adelaide.edu.au>
2025-06-20 01:04:20 +0000

    [automatic commit] 2025-06-20 01:04:20 UTC

--- part2/app.js
+++ part2/app.js
@@ -16,7 +16,6 @@ app.use('/api/walks', walkRoutes);
 app.use('/api/users', userRoutes);
 
 // Export the app instead of listening here
-module.exports = app;
 const app = require('./app');
 const port = 8080;
 
