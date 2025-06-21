428896e82b622c697b4529703b7f5e0ed78da71f
WDC Student (missing default git user name) <unknown@student.adelaide.edu.au>
2025-06-21 00:44:49 +0000

    [automatic commit] 2025-06-21 00:44:49 UTC

--- part1/dogwalks.sql
+++ part1/dogwalks.sql
@@ -1,6 +1,10 @@
 DROP DATABASE IF EXISTS DogWalkService;
 CREATE DATABASE DogWalkService;
 USE DogWalkService;
+DROP DATABASE IF EXISTS dogwalks;
+CREATE DATABASE dogwalks;
+USE dogwalks;
+
 CREATE TABLE Users (
     user_id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(50) UNIQUE NOT NULL,
